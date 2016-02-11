6ced1492f84bdfc190afee64dbad90c8d15bf076
Andy Malik <me@andymalik.com>
2016-02-10 23:29:56 -0800

    I've went through the readings, I learned up on the docs, I know the purpose of all that I am doing, I just have no clue how to implement it. I worked until Dinner, took a break, and came back. I was able to setup the cells with autolayout so I can see the entire tweets text, and I was able to 'link' a tweet on the home view nav with a second Nav Controller, to fade to a template page, but I couldn't figure out how to get past that.

--- TweetCloneHomework/TweetViewController.swift
+++ TweetCloneHomework/TweetViewController.swift
@@ -9,28 +9,24 @@
 import UIKit
 
 class TweetViewController: UIViewController {
-
+    
+    @IBOutlet weak var singleTweet: UILabel!
+    
+    var tweet: Tweet?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        setupTweet()
     }
     
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupTweet()
+    {
+        if let tweet = self.tweet {
+            self.singleTweet.text = tweet.text
+        }
     }
-    */
-
 }
